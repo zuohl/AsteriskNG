f9c4200e1a31a5c56654ef165245800da3cfd3f3
Whale Choi <cj1369636717@gmail.com>
2026-05-26 23:35:19 +0800

    bump version 1.0.0

--- app/src/main/kotlin/features/subscription/SubscriptionDefaults.kt
+++ app/src/main/kotlin/features/subscription/SubscriptionDefaults.kt
@@ -1,6 +1,6 @@
 package features.subscription
 
 const val DefaultSubscriptionGroupId = 1
-const val DefaultSubscriptionUserAgent = "v2rayNG/2.1.8"
+const val DefaultSubscriptionUserAgent = "v2rayNG/2.2.0"
 const val AutoSubscriptionCheckIntervalMillis = 60L * 1000L
 const val AutoSubscriptionRetryDelayMillis = 15L * 60L * 1000L
