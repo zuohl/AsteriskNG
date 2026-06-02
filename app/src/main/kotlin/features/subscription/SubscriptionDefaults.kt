c2a4e53a874b7fe7f0f4025bba01f54a6e953e7f
Whale Choi <cj1369636717@gmail.com>
2026-06-03 01:51:27 +0800

    core: bump AndroidLibXrayLite to v26.6.2

--- app/src/main/kotlin/features/subscription/SubscriptionDefaults.kt
+++ app/src/main/kotlin/features/subscription/SubscriptionDefaults.kt
@@ -4,9 +4,9 @@
 package features.subscription
 
 const val DefaultSubscriptionGroupId = 1
-const val DefaultSubscriptionUserAgent = "v2rayNG/2.2.2"
+const val DefaultSubscriptionUserAgent = "v2rayNG/2.2.3"
 const val ClashMetaSubscriptionUserAgent = "clash.meta"
-const val FlClashXSubscriptionUserAgent = "FlClash X/v0.4.0-pre.12 Platform/android"
+const val FlClashXSubscriptionUserAgent = "FlClash X/v0.4.0-pre.15 Platform/android"
 const val AutoSubscriptionCheckIntervalMillis = 60L * 1000L
 const val AutoSubscriptionRetryDelayMillis = 15L * 60L * 1000L
 
